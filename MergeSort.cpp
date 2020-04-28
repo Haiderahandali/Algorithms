d4b3f286087d81529a509fccc3f5acb0646d0abd
Ali A <53301143+Haiderahandali@users.noreply.github.com>
2020-04-28 13:49:00 +0300

    Merge_SORT

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,92 +1,44 @@
-#include <iostream>
-
-void Merge(int array[], int p, int left, int  right);// Merge Function
-void Merge_Sort(int array[], int left, int  right); //actual merge sort
-
-int main() {
-
-  //input, unsorted array>
-  int arr[9]{9,-5,-2,40,41,5,6,5,19};
-
-  //merge sort the array of boundaries 0 ->8 inclusive
-   Merge_Sort(arr, 0, 8);
-  
-
-  //printing the array
-  for(auto x : arr)
-    std::cout<<x<<std::endl;
-}
+#include<iostream>
+#include<vector>
 
 
 
-void Merge_Sort(int array[], int left, int  right)
+
+
+void merge (std::vector<int> v1,std:: vector<int> v2) 
 {
-  //if the array is empty or of one element, we're done.
-  if(left<right)
-  {
-    //speicify mid point p to separate left & right
-    int p = (left+right)/2;
-
-    //merge sort the left -> mid point
-    Merge_Sort(array, left,p);
-
-    //merge sort mid + 1  -> right
-    Merge_Sort(array,p+1,right);
-
-    //merge both the left and the right.
-    Merge(array,p,left,right);
-
-  }
-
-}
-
-void Merge(int array[], int p,int left, int  right)
-{
-  
-  //init new arrays to have the left and right side
-  // we add an extra sentile element to each array (infinity) at line 75 
-  int L[p - left + 1 + 1]; 
-  int R[right-p+1];
-
-  //setting the boundaries for each new array (L and R)
-  int n1 = p-left;
-  int n2 = right - p;
-
-
-  //getting elemetns from oringal array to each array
-  for(int i =0;i<=n1;i++)
-  {
-    L[i] = array[left+i];
-  }
-  
-  //same here.
-  for(int j =0; j<n2 ;j++)
-  {
-    R[j]= array[p +1 + j];      
-  }
-
-  
-  L[n1+1] = 1000000; //the extra sentile element at the end
-  R[n2] = 1000000;
-
-  //Merging the 2 arrays
-  int i =0; int j = 0;
-  for (int k = left;k<right+1;k++)
-  {
+std::vector<int> MRG;
+   
+    int i,j;
+    i = j = 0;
+    int s1 = v1.size() ;
+    int s2 = v2.size() ;
     
-    if (L[i]< R[j])
-    {
-      array[k] = L[i];
-      ++i;
+    //MRG.reserve(v1.size()+ v2.size() - 2);
+   while (int n = s1+s2)
+   {
+    std::cout << n << std::endl;
+        if (v1[i] > v2[j])
+        {
+            MRG.push_back(v2[j]);
+            j++;
+            n--;
+        }
+        else
+        {
+            MRG.push_back(v1[i]);
+            i++;
+            n--;
+        }
+        
+   
+    
     }
-    else 
-    {
-      array[k] = R[j];
-      ++j;
-    }
-      
-  // because we add an extra element at the end, we don't need to check if the L, R arrays are empty. 
-  
-  }
-  
+}
+
+int main(){
+std::vector<int> arr(6,0);
+int a[6]{0};
+
+
 }
